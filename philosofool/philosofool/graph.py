1bae6ce077774ab0ed11cd98d43ec352a9eca67b
Stephen Lenhart <stephen.lenhart@lockerdome.com>
2025-06-06 16:45:27 -0700

    Introduce Node, Edge, Graph

---
+++ philosofool/philosofool/graph.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+from collections.abc import Hashable, Sequence
+
+
+class Node:
+    def __init__(self, name: Hashable):
+        self.name = name
+        self.edges = set()
+
+    def add_edge(self, edge: Edge):
+        self.edges.add(edge)
+
+    def remove_edge(self, edge: Edge):
+        self.edges.remove(edge)
+
+    def adjacent_nodes(self):
+        return {edge.node for edge in self.edges}
+
+    def edges_to_node(self, node: Node):
+        return {edge for edge in self.edges if edge.node == node}
+
+    def disconnect(self, node: Node):
+        for edge in self.edges_to_node(node):
+            self.remove_edge(edge)
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return False
+        return (self.name == other.name) and (self.edges == other.edges)
+
+
+class Edge:
+    def __init__(self, node: Node, label):
+        self.node = node
+        self.label = label
+
+    def __hash__(self):
+        return hash((self.node, self.label))
+
+    def __eq__(self, other):
+        if not isinstance(other, Edge):
+            return False
+        return self.node == other.node and self.label == other.label
+
+
+class Graph:
+    def __init__(self):
+        self.nodes: dict[Hashable, Node] = {}
+
+    def add_node(self, node: Node):
+        if node.name in self.nodes and self.nodes[node.name] != node:
+            raise ValueError(f"Attempt to add {node} to graph, but it is not equivalent to {self.nodes[node.name]}")
+        self.nodes[node.name] = node
+        for edge_node in node.adjacent_nodes():
+            if edge_node not in self:
+                self.add_node(edge_node)
+
+    def get_node(self, node):
+        return self.nodes[node.name]
+
+    def remove_node(self, node: Node):
+        """Remove node from graph and delete any edge from another node to that one."""
+        for other_node in self.nodes.values():
+            node.disconnect(other_node)
+            other_node.disconnect(node)
+        self.nodes.pop(node.name)
+
+    def depth_first_search(self, node: Node | None):
+        if node is None:
+            arbitrary_node = next(iter(self.nodes.values()))
+            return self.depth_first_search(arbitrary_node)
+
+    @classmethod
+    def from_dict(cls, nodes: dict[Hashable, Sequence]):
+        nodes_cache = {}
+        graph = cls()
+
+        def get_cached_node(node_name) -> Node:
+            node = nodes_cache.get(node_name, Node(node_name))
+            if node_name not in nodes_cache:
+                nodes_cache[node_name] = node
+            return node
+
+        for node_name, edges in nodes.items():
+            node = get_cached_node(node_name)
+            for edge_name, label in edges:
+                edge_node = get_cached_node(edge_name)
+                edge = Edge(edge_node, label)
+                node.add_edge(edge)
+            graph.add_node(node)
+
+        return graph
+
+    def __contains__(self, node: Node):
+        if not isinstance(node, Node):
+            return False
+        return self.nodes.get(node.name) is not None
---
+++ philosofool/tests/test_graph.py
@@ -0,0 +1,200 @@
+import pytest
+
+import numpy as np
+from philosofool.graph import Node, Edge, Graph
+
+def test_node_attributes():
+    node = Node('a')
+    assert node.name == 'a'
+    assert hash(node) == hash(node.name)
+    assert isinstance(node.edges, set)
+
+def test_node_equality():
+    assert Node('a') == Node('a')
+    assert Node('a') != Node('b')
+
+def test_node_add_to_set():
+    a_set = set()
+    for i in range(2):
+        a_set.add(Node(1))
+    assert len(a_set) == 1
+    a_set.remove(Node(1))
+    assert not a_set
+
+def test_edge_add_to_set():
+    a_set = set()
+    for i in range(2):
+        a_set.add(Edge(Node(1), 1))
+    assert len(a_set) == 1
+    a_set.remove(Edge(Node(1), 1))
+    assert not a_set
+
+def test_add_edge():
+    node = Node('a')
+    edge = Edge(node, 'self')
+    node.add_edge(edge)
+    assert len(node.edges) == 1
+    assert edge in node.edges, "The edge should be in the node edges."
+    node.add_edge(edge)
+    assert len(node.edges) == 1, "Adding the same edge twice has no effect."
+
+def test_remove_edge():
+    node = Node('a')
+    edge = Edge(node, 'self')
+    node.add_edge(edge)
+    node.remove_edge(edge)
+    assert edge not in node.edges
+
+def test_adjacent_nodes():
+    node = Node(0)
+    assert node.adjacent_nodes() == set()
+    node.add_edge(Edge(Node(1), '1'))
+    assert node.adjacent_nodes() == {Node(1)}
+    node.add_edge(Edge(node, None))
+    assert node.adjacent_nodes() == {Node(1), node}
+
+def test_disconnect():
+    """Remove all edges leading to node."""
+    node1 = Node(1)
+    node2 = Node(2)
+    first_edge = Edge(node2, 'first')
+    second_edge = Edge(node2, 'second')
+    node1.add_edge(first_edge)
+    node1.add_edge(second_edge)
+    node1.disconnect(node2)
+    assert node2 not in node1.adjacent_nodes()
+
+def test_edges_to_node():
+    node1 = Node(1)
+    node2 = Node(2)
+    first_edge = Edge(node2, 'first')
+    second_edge = Edge(node2, 'second')
+    node1.add_edge(first_edge)
+    node1.add_edge(second_edge)
+    assert node1.edges_to_node(node2) == {first_edge, second_edge}
+
+def test_edge_attributes():
+    edge = Edge(Node('a'), label=None)
+    assert edge.node == Node('a')
+    assert edge.label is None
+
+def test_edge_equality():
+    node = Node('node')
+    edge = Edge(node, 'label')
+    assert edge == Edge(node, 'label')
+    assert edge != Edge(node, 'different label')
+    assert edge != Edge(Node('other_node'), 'label')
+    assert edge != Edge(Node('other node'), 'different label')
+
+def test_graph():
+    graph = Graph()
+    assert graph.nodes == {}
+
+def test_add_node():
+    graph = Graph()
+    assert graph.nodes == {}
+    node = Node(1)
+    graph.add_node(node)
+    assert graph.nodes == {node.name: node}
+
+    node2 = Node(2)
+    node3 = Node(3)
+    node2.add_edge(Edge(node3, '3'))
+    graph.add_node(node2)
+    assert node2 in graph
+    assert node3 in graph
+
+    similar_node = Node(1)
+    similar_node.add_edge(Edge(similar_node, 'self'))
+    with np.testing.assert_raises(ValueError, msg='Adding a node with the same label that is not equivalent should be prohibited.'):
+        graph.add_node(similar_node)
+
+def test_graph_add_edge_to_graphed_node():
+    graph = Graph()
+    node1 = Node(1)
+    graph.add_node(node1)
+    node2 = Node(2)
+    edge = Edge(node2, '2')
+    node1.add_edge(edge)
+    assert node2 not in graph, "Adding a edge to node in a graph will not add that edge-node to the graph."
+    graph.add_node(node1)
+    assert node2 in graph, "Redundantly adding a node to a graph will add missing edge-nodes."
+
+def test_get_node():
+    graph = Graph()
+    graph.add_node(Node(1))
+    assert graph.get_node(Node(1)) == Node(1)
+    with np.testing.assert_raises(KeyError):
+        assert graph.get_node(Node(0))
+
+def test_in_graph():
+    graph = Graph()
+    assert 1 not in graph
+    assert Node(1) not in graph
+    graph.add_node(Node(1))
+    assert 1 not in graph
+    assert Node(1) in graph
+
+def test_remove_node():
+    graph = Graph()
+    graph.add_node(Node(1))
+    graph.remove_node(Node(1))
+    assert Node(1) not in graph
+
+    node1 = Node(1)
+    node2 = Node(2)
+    # node3 = Node(3)
+    node1.add_edge(Edge(node2, 'first'))
+    node1.add_edge(Edge(node2, 'second'))
+    node2.add_edge(Edge(node1, 'first'))
+    graph.add_node(node1)
+    graph.add_node(node2)
+    graph.remove_node(node1)
+    assert node1 not in node2.adjacent_nodes()
+    assert node2 not in node1.adjacent_nodes()
+    assert node1 not in graph
+
+@pytest.fixture
+def graph() -> Graph:
+    graph = Graph.from_dict({
+        1: [(2, '2'), (3, '3')],
+        3: [(4, '4')]
+    })
+    return graph
+
+@pytest.fixture
+def nodes(graph):
+    return [graph.nodes[i] for i in range(1, 5)]
+
+def test_from_dict_creates_nodes(graph):
+    assert isinstance(graph, Graph)
+    assert len(graph.nodes) == 4
+    node1, node2, node3, node4 = graph.nodes.values()
+    for node in node1, node2, node3, node4:
+        assert isinstance(node, Node)
+
+def test_from_dict_adjacent_nodes(nodes):
+    node1, node2, node3, node4 = nodes
+
+    assert node2 in node1.adjacent_nodes()
+    assert node3 in node1.adjacent_nodes()
+    assert node4 not in node2.adjacent_nodes()
+
+    assert node4 in node3.adjacent_nodes()
+    assert not node4.adjacent_nodes()
+    assert not node2.adjacent_nodes()
+
+def test_from_dict_edges(nodes):
+    node1, node2, node3, node4 = nodes
+
+    assert any(Edge(node2, '2') == edge for edge in node1.edges)
+    assert any(Edge(node3, '3') == edge for edge in node1.edges)
+    assert len(node1.edges) == 2
+    assert any(Edge(node4, '4') == edge for edge in node3.edges)
+    assert len(node3.edges) == 1
+    assert not node4.edges
+    assert not node2.edges
+
+
+def test_depth_first_search():
+    raise Exception("This test has not been written.")
